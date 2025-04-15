197595fcb8448966d4c49677d5013ecc1e4c1fb2
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-15 21:45:30 +0600

    Router Basic Practice Done

---
+++ src/Components/Mobile/Mobile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobile = () => {
+    return (
+        <div>
+            <h1>This is Mobile</h1>
+        </div>
+    );
+};
+
+export default Mobile;<h1>This is Mobile</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Components/Root/Root.jsx'
+import Mobile from './Components/Mobile/Mobile.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+
+const router = createBrowserRouter([
+  {path: '/', Component: Root,
+    children: [
+      { index: true, Component: Mobile },
+      { path: 'about', Component: About},
+      { path: 'contact', Component: Contact }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
