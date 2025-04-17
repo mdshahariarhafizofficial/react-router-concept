799881a17922502d52c1ef20d55aba890cbc6a37
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-17 13:10:09 +0600

    404 not found added

--- src/Components/Comments/Comment.jsx
+++ src/Components/Comments/Comment.jsx
@@ -9,14 +9,14 @@ const Comment = ({comment}) => {
     .then(res => res.json());
 
     return (
-        <div className='border-2 border-amber-400 p-5 rounded-2xl flex flex-col justify-between gap-6'>
+        <div className='border-2 border-amber-400 p-5 rounded-2xl flex flex-col justify-between gap-3'>
             <h2 className='uppercase font-bold text-teal-500'>{name}</h2>
             <p>{email}</p>
             <Link to={`/comments/${id}`}>
                 <button className="btn btn-success">Show Comment Details</button>
             </Link>
             <button onClick={()=> setShowComment(!showComment)} className="btn btn-success">{showComment?"Hide":"Show"} Comment</button>
-            <Suspense fallback={<h1>Loading....</h1>}>
+            <Suspense fallback={<span className="loading loading-spinner text-warning"></span>}>
                 {
                     showComment&& <ShowComment commentFetch={commentFetch}></ShowComment>
                 }
--- src/main.jsx
+++ src/main.jsx
@@ -58,6 +58,10 @@ const router = createBrowserRouter([
         Component: CommentsDetails
       },
     ]
+  },
+  {
+    path: '*',
+    element: <h1>404! not found</h1>
   }
 ])
 
